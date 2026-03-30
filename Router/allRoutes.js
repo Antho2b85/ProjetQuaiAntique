a6c90b0aea35246deceef4b6eb1861bfe28e2ff7
Anthony Mara <anthony.mara@hotmail.fr>
2026-03-30 14:58:55 +0200

    Ajout de page signin

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,10 @@ import Route from "./Route.js";
 
 // Définir maintenant les routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+];
 
     // Le titre va s'afficher comme ceci : Route.titre - websitename
     export const websiteName = "Quai Antique";
